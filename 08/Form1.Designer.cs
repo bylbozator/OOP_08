35d63f46d26d79456d0472003e2fe3c8eca683e6
ya-molodez <nstmhlva@gmail.com>
2024-01-09 20:24:40 +0500

    Добавьте файлы проекта.

---
+++ 08/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace _08
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(267, 228);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(189, 120);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightBlue;
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ButtonFace;
+            button1.Location = new Point(31, 228);
+            button1.Name = "button1";
+            button1.Size = new Size(160, 120);
+            button1.TabIndex = 1;
+            button1.Text = "Пуск";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.TopLeft;
+            dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.BackgroundColor = Color.SkyBlue;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(31, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(425, 184);
+            dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(613, 375);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Лаб. №8";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ 08/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System.Drawing.Drawing2D;
+namespace _08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = 5; // Устанавливаем количество строк в dataGridView1
+            dataGridView1.ColumnCount = 5; // Устанавливаем количество столбцов в dataGridView1
+
+            int[,] a = new int[5, 5]; // Инициализируем двумерный массив размером 5x5
+            int i, j;
+
+            // Заполняем матрицу случайными числами
+            Random rand = new Random();
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    a[i, j] = rand.Next(-100, 100); // Генерируем случайное число и помещаем его в матрицу
+                }
+            }
+
+            // Выводим матрицу в dataGridView1
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = a[i, j].ToString(); // Выводим элементы матрицы в ячейки dataGridView1
+                }
+            }
+
+            int minColumn = 0; // Индекс столбца с наименьшим количеством положительных элементов
+            int minCount = int.MaxValue; // Количество положительных элементов в столбце с наименьшим количеством
+
+            // Находим столбец с наименьшим количеством положительных элементов
+            for (int column = 0; column < 5; column++)
+            {
+                int count = 0; // Счетчик положительных элементов в столбце
+
+                for (int row = 0; row < 5; row++)
+                {
+                    if (a[row, column] > 0)
+                    {
+                        count++; // Увеличиваем счетчик, если элемент положительный
+                    }
+                }
+
+                if (count < minCount)
+                {
+                    minColumn = column; // Обновляем индекс столбца с наименьшим количеством
+                    minCount = count; // Обновляем количество положительных элементов
+                }
+            }
+
+            textBox1.Text = Convert.ToString("Номер столбца с наименьшим количеством положительных элементов (отсчёт с нуля): " + minColumn);
+        }
+    }
+}
